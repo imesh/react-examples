b2fb5430e614cacb20f30e052a93800b1317a42e
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2018-06-19 18:47:32 +0530

    First revision of user login example

---
+++ user-login/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+  </div>
+);
+
+export default App;
